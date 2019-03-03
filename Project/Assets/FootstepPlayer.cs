190d53883612bb4215690d8375c47e188b18e578
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-03-03 21:02:35 +0100

    footsteps for player

---
+++ Project/Assets/FootstepPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepPlayer : MonoBehaviour
+{
+	[SerializeField] private GameObject footstepPrefab;
+	[SerializeField] private Transform[] footPositions;
+
+	private AudioSource audioSource;
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void TriggerFootstep(int footID)
+	{
+		Instantiate(footstepPrefab, footPositions[footID].position, Quaternion.identity);
+	}
+}
