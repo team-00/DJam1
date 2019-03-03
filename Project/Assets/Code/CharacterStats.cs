5691f1ff27ef8f15aea39e86119f11381dbd78ae
YDenker <33430355+YDenker@users.noreply.github.com>
2019-03-03 15:22:12 +0100

    User Interface

---
+++ Project/Assets/Code/CharacterStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats
+{
+    private float sanity, hunger; // 0 = INSANE & 100 = HEALTHY
+
+    internal float Sanity
+    {
+        get => sanity;
+        set
+        {
+            sanity = value;
+            panel.Sanity = value;
+        }
+    }
+    internal float Hunger
+    {
+        get => hunger;
+        set
+        {
+            hunger = value;
+            panel.Hunger = value;
+        }
+    }
+
+    private CharacterStatsPanel panel;
+
+    internal CharacterStats()
+    {
+        hunger = 100;
+        sanity = 100;
+    }
+
+    internal void ConnectPanel(CharacterStatsPanel panel)
+    {
+        this.panel = panel;
+    }
+}
---
+++ Project/Assets/Code/RenderTexture/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    internal float RotationSpeed = 20f;
+
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * RotationSpeed, 0);    
+    }
+}
