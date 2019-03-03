f7f5f499e8bcb68c4ea35c5022b1c2d0684f4881
YDenker <33430355+YDenker@users.noreply.github.com>
2019-03-03 21:52:16 +0100

    Menu complete

---
+++ Project/Assets/Code/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject UserInterface, MenuInterface, Controls;
+
+    private void Start()
+    {
+        UserInterface.SetActive(false);
+        Controls.SetActive(false);
+    }
+
+    public void GameStart()
+    {
+        UserInterface.SetActive(true);
+        MenuInterface.SetActive(false);
+    }
+    public void DisplayControls()
+    {
+        Controls.SetActive(!Controls.activeInHierarchy);
+    }
+
+    public void DisplayCredits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
